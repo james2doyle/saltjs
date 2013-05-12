c0618f04807aceab1d788597ba2d4e786ab4e4aa
james2doyle <james2doyle@gmail.com>
2013-05-12 18:10:47 -0400

    updated salt with attribution

--- salt.js
+++ salt.js
@@ -1,21 +1,12 @@
+/*! Salt.js DOM Selector Lib. By @james2doyle */
 window.$ = function(_s) {
   var m = {
-    '#': function(_q) {
-      return document.getElementById(_q);
-    },
-    '.': function(_q) {
-      return document.getElementsByClassName(_q);
-    },
-    '@': function(_q) {
-      return document.getElementsByName(_q);
-    },
-    '=': function(_q) {
-      return document.getElementsByTagName(_q);
-    },
-    '*': function(_q) {
-      return document.querySelectorAll(_q);
-    }
+    '#': 'getElementById',
+    '.': 'getElementsByClassName',
+    '@': 'getElementsByName',
+    '=': 'getElementsByTagName',
+    '*': 'querySelectorAll'
   };
   var t = /[=#@.*]/.exec(_s)[0];
-  return [m[t](_s.split(t)[1])][0];
+  return (document[m[t]](_s.split(t)[1]));
 };
