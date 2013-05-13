aa50b2e12b36b291d89137b7d9552f04085de14f
james2doyle <james2doyle@gmail.com>
2013-05-12 23:23:18 -0400

    annotate source

--- salt.js
+++ salt.js
@@ -1,12 +1,17 @@
 /*! Salt.js DOM Selector Lib. By @james2doyle */
-window.$ = function(_s) {
-  var m = {
+window.$ = function(selector) {
+  // an object containing the matching keys and native get commands
+  var matches = {
     '#': 'getElementById',
     '.': 'getElementsByClassName',
     '@': 'getElementsByName',
     '=': 'getElementsByTagName',
     '*': 'querySelectorAll'
   };
-  var t = /[=#@.*]/.exec(_s)[0];
-  return (document[m[t]](_s.split(t)[1]));
+  // regex function to split the command with the target
+  // so $('#iddiv') would return '#'
+  var regex = /[=#@.*]/.exec(selector)[0];
+  // m[t] returns the command. use that in the document object
+  // now pass the target without the match key
+  return (document[matches[regex]](selector.split(regex)[1]));
 };
