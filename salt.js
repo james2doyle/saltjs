f89eade1242c5eb74713f34b7eb86c06582a029e
james2doyle <james2doyle@gmail.com>
2013-05-15 12:46:27 -0400

    change query selector ? to *. use split on string. move regex into matches

--- salt.js
+++ salt.js
@@ -7,11 +7,10 @@ window.$ = function(selector) {
     '@': 'getElementsByName',
     '=': 'getElementsByTagName',
     '*': 'querySelectorAll'
-  };
+  }[/[=#@.*]/.exec(selector)[0]];
   // regex function to split the command with the target
   // so $('#iddiv') would return '#'
-  var regex = /[=#@.*]/.exec(selector)[0];
   // m[t] returns the command. use that in the document object
   // now pass the target without the match key
-  return (document[matches[regex]](selector.split(regex)[1]));
+  return (document[matches](selector.slice(1)));
 };
