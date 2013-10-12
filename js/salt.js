a4c75c9de172f665288153930d505366a9dd9446
James Doyle <james2doyle@gmail.com>
2013-10-12 13:33:46 -0400

    added context and undefined

--- js/salt.js
+++ js/salt.js
@@ -1,5 +1,5 @@
 /*! Salt.js DOM Selector Lib. By @james2doyle */
-window.$ = function(selector) {
+window.$ = function(selector, context, undefined) {
   // an object containing the matching keys and native get commands
   var matches = {
     '#': 'getElementById',
@@ -9,7 +9,7 @@ window.$ = function(selector) {
     '*': 'querySelectorAll'
   }[selector[0]]; // you can treat a string as an array of characters
   // now pass the selector without the key/first character
-  var el = (document[matches](selector.slice(1)));
+  var el = (((context === undefined) ? document: context)[matches](selector.slice(1)));
   // if there is one element than return the 0 element
   return ((el.length < 2) ? el[0]: el);
 };
